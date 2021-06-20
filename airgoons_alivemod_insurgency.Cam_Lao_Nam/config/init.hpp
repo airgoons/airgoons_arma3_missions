58cb9a99b3f3915f92f9a2e6272ed59392f3db63
wonkotron <wonkotron@gmail.com>
2021-06-19 17:33:32 -0700

    Initial mission scripting

---
+++ airgoons_alivemod_insurgency.Cam_Lao_Nam/config/init.hpp
@@ -0,0 +1,9 @@
+// Arsenal Configuration
+#include "arsenal.hpp"
+// TFAR Configuration
+#include "tfar.hpp"
+// Wheel Menu Actions
+#include "wheel_menu_actions.hpp"
+
+// load profile namespace variables for runtime use
+__EXEC(allProfileNamespaceVars = allVariables profileNamespace);
